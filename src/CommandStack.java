7fb3e8b494395f685b1328d4892dbd9fd1e02b93
dpjain <dpjain@iu.edu>
2022-05-26 15:20:45 +0530

    Infix to Postfix

--- src/CommandStack.java
+++ src/CommandStack.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class CommandStack implements Strategy{
 
@@ -26,14 +28,92 @@ public class CommandStack implements Strategy{
     @Override
     public String parse_expression(String expression) {
 
+        StringTokenizer tokenizer = new StringTokenizer(expression);
+        String token;
+        String postfix = "";
+        Stack<String> stack = new Stack<>();
 
+        while(tokenizer.hasMoreTokens()){
+            token = tokenizer.nextToken();
 
-        return null;
+            if(isFloat(token)){
+                stack.push(token);
+            }
+
+            else if(token.equals("(")){
+                stack.push(token);
+            }
+
+            else if(token.equals(")")){
+                while(!stack.peek().equals("(")){
+                    postfix = postfix + stack.peek() + " ";
+                    stack.pop();
+                }
+
+                stack.pop();
+
+            }
+
+            else if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/") || token.equalsIgnoreCase("SQRT") || token.equalsIgnoreCase("CBRT")){
+                while(!stack.empty() && operatorPrecedence(token) <= operatorPrecedence(stack.peek())){
+                    postfix = postfix + stack.peek() + " ";
+                    stack.pop();
+                }
+
+                stack.push(token);
+            }
+
+            else {
+                throw new IllegalStateException();
+            }
+        }
+
+        while(!stack.empty()) {
+            postfix = postfix + stack.peek() + " ";
+            stack.pop();
+        }
+
+        return postfix;
     }
 
     @Override
     public int evaluate_expression(String parse) {
-        return 0;
+
+        int result;
+
+        return result;
+
+    }
+
+    private boolean isFloat(String str){
+        try{
+            Float.parseFloat(str);
+            return true;
+        }
+        catch(NumberFormatException exception){
+            return false;
+        }
+    }
+
+    private int operatorPrecedence(String str){
+        if(str.equalsIgnoreCase("SQRT") || str.equalsIgnoreCase("CBRT")){
+            return 3;
+        }
+
+        else if(str.equals("*") || str.equals("/")){
+            return 2;
+        }
+
+        else if(str.equals("+") || str.equals("-")){
+            return 1;
+        }
+
+        else if(isFloat(str)){
+            return 5;
+        }
+
+	else
+        return -1;
     }
 
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,16 +1,11 @@
-import java.util.Stack;
-
 public class Driver {
 
     public static void main(String args[]){
-        Stack<Float> stack = new Stack<>();
-        Command num1 = new Number_Command(6);
-        Command num2 = new Number_Command(729);
-        num1.execute(stack);
-        num2.execute(stack);
-        Command a = new Cbrt_Command();
-        a.execute(stack);
-        System.out.println(stack.peek());
+
+        CommandStack cs = new CommandStack();
+        String str = cs.parse_expression("9 - ( 32 + 23 ) * 21 / 2");
+        System.out.println(str);
+
     }
 
 }
